c7d1a1a8f76d24a1892e7ac13093276f4a6abd41
Matthew Chang <matthewchang04@gmail.com>
2025-07-01 13:43:18 -0400

    added to CMakeLists

---
+++ build/CMakeFiles/tool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tool.dir/src/ClangASTExtractor.cpp.o"
+  "CMakeFiles/tool.dir/src/ClangASTExtractor.cpp.o.d"
+  "tool"
+  "tool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
