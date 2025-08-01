d44e9a8e6f1fdc6bc3729c7f1605668fbe4b9fd5
Matthew Chang <matthewchang04@gmail.com>
2025-07-31 22:34:45 -0400

    created build.sbt

--- CPGTrimmer/src/test/scala/io/CPGTrimmerTests.scala
+++ CPGTrimmer/src/test/scala/io/CPGTrimmerTests.scala
@@ -2,6 +2,7 @@ import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.semanticcpg.testing.MockCpg
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import java.nio.file.{Files, Paths}
 
 
 // TODO: keep working on basic test implementation (get the test.c file liked (or find an easier way))
@@ -9,8 +10,9 @@ import org.scalatest.wordspec.AnyWordSpec
 class CPGTrimmerPassTests extends AnyWordSpec with Matchers {
   
   "CPGTrimmerPass" should {
-    "Remove all empty BLOCK Nodes" in {
-
+   "Remove all empty BLOCK Nodes" in {
+	   val dir = "resources/test.c"
+	   val 
     }
     
     "handle empty CPG gracefully" in {
