97bf4f3c03547ec95be2bd8401cd94206032a7ec
Matthew Chang <matthewchang04@gmail.com>
2025-08-07 23:06:25 -0400

    built up type pass

--- CPPTypeRecoveryPlugin/src/main/scala/io/joern/c2cpgTypeRecovery/CPPTypeRecoveryPass.scala
+++ CPPTypeRecoveryPlugin/src/main/scala/io/joern/c2cpgTypeRecovery/CPPTypeRecoveryPass.scala
@@ -4,11 +4,9 @@ import io.shiftleft.codepropertygraph.Cpg
 import overflowdb.BatchedUpdate.DiffGraphBuilder
 import io.shiftleft.passes.CpgPass
 import io.shiftleft.semanticcpg.language.*
+import io.shiftleft.codepropertygraph.generated.nodes.*
+import io.shiftleft.codepropertygraph.generated.PropertyNames
 
-
-object CPPTypeRecoveryPass {
-
-}
 
 class CPPTypeRecoveryPass(cpg: Cpg) extends CpgPass(cpg) {
 
@@ -19,23 +17,39 @@ class CPPTypeRecoveryPass(cpg: Cpg) extends CpgPass(cpg) {
   def fixAmbiguousCalls(dstGraph: DiffGraphBuilder): Unit = {
     val calls = cpg.call.toList
     val operatorCalls = calls.filter(call => call.methodFullName.startsWith("<operator"))
-    val otherAmbiguous = calls.filter(call=> call.typeFullName == "ANY")
+    val otherAmbiguous = calls.filter(call=> !call.methodFullName.startsWith("<operator") && call.typeFullName == "ANY")
     
-    fixOperatorCalls(dstGraph)
-    fixOtherAmbiguousCalls(dstGraph)
+    fixOperatorCalls(operatorCalls, dstGraph)
+    fixOtherAmbiguousCalls(otherAmbiguous, dstGraph)
   }
 
-  def fixOperatorCalls(dstGraph: DiffGraphBuilder) :Unit = {
-
+  def fixOperatorCalls(operatorCalls: List[Call], dstGraph: DiffGraphBuilder) :Unit = {
+    val assignmentCalls = operatorCalls.filter(call => call.methodFullName.endsWith("assignment"))
+    val pointerDerefCalls = operatorCalls.filter(call => call.methodFullName.endsWith("pointer"))
+    
+    // TO IMPLEMENT:
+    /*
+      indirectIndexAccess
+      Addition, Multiplication, Subtraction, etc
+      Cast
+      
+     */
   }
 
-  def fixOtherAmbiguousCalls(dstGraph: DiffGraphBuilder): Unit = {
+  def fixAssignmentCalls(assignmentCalls: List[Call], dstGraph: DiffGraphBuilder) = {
+    assignmentCalls.foreach(call => {
+      val identifiers: Iterator[Identifier] = call.astChildren.isIdentifier.cast[Identifier]
+      val assignee: Identifier = identifiers.argumentIndex(1).head
+
+      dstGraph.setNodeProperty(call, PropertyNames.TYPE_FULL_NAME, assignee.typeFullName)
+    })
+  }
+
+  def fixOtherAmbiguousCalls(otherAmbiguous: List[Call], dstGraph: DiffGraphBuilder): Unit = {
 
   }
 
   def cleanPointerTypes(dstGraph: DiffGraphBuilder): Unit = {
 
   }
-
-  def
 }
