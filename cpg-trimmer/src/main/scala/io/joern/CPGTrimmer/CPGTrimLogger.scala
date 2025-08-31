60cf8daa89d96a5f81317ac5cc09e37a8cf7457d
Matthew Chang <matthewchang04@gmail.com>
2025-08-30 22:12:04 -0400

    fixes to the logger pass

--- cpg-trimmer/src/main/scala/io/joern/CPGTrimmer/CPGTrimLogger.scala
+++ cpg-trimmer/src/main/scala/io/joern/CPGTrimmer/CPGTrimLogger.scala
@@ -3,12 +3,12 @@ package io.joern.CPGTrimmer
 import io.shiftleft.semanticcpg.language.*
 import io.shiftleft.codepropertygraph.generated.{Cpg, Properties}
 import io.shiftleft.codepropertygraph.generated.nodes
-import org.slf4j.LoggerFactory
 import flatgraph.GNode
 import io.shiftleft.codepropertygraph.generated.nodes.StoredNode
+import java.io.PrintWriter
 
-class CPGTrimLogger(emptyBlocks: List[GNode], cpg: Cpg) {
-  private val logger = LoggerFactory.getLogger(getClass[CPGTrimLogger])
+
+class CPGTrimLogger(emptyBlocks: List[GNode], cpg: Cpg, fileWriter: Option[PrintWriter]) {
 
   def logNodes(): Unit = {
     emptyBlocks.foreach(node => logNodeDetails(node))
@@ -16,36 +16,45 @@ class CPGTrimLogger(emptyBlocks: List[GNode], cpg: Cpg) {
 
   private def logNodeDetails(node: GNode): Unit = {
     val text = getNodeString(node)
-    logger.info(text)
+
+    fileWriter.foreach { writer =>
+      writer.println(text)
+      writer.println("-" * 50)
+      writer.flush()
+    }
+
   }
 
   private def getNodeString(node: GNode): String = {
 
-    // TODO: FIND GNODE equivalent of _astIn
     val nodeContext: StoredNode = cpg.id(node.id()).head
 
     val parentsList: List[String] = nodeContext._astIn.toList.map(snode =>
       s"""
-         |Parent ID = ${snode.id()}
-         |  Parent Type = ${snode.label()}
-         |""".stripMargin
+         |{
+         |  Parent ID: ${snode.id()}
+         |  Parent Type: "${snode.label()}"
+         |  Parent Name: "${snode.property(Properties.FullName)}"
+         |  Parent Code: "${snode.property(Properties.Code)}"
+         |}""".stripMargin
     )
 
     val childrenList: List[String] = nodeContext._astOut.toList.map(node => {
       s"""
-         |Child ID = ${node.id()}
-         |  Child Type = ${node.label}
-         |""".stripMargin
+         |{
+         |  Child ID: ${node.id()},
+         |  Child Type = "${node.label()}",
+         |}""".stripMargin
     })
 
     val text =
       s"""
-         |Node ID = ${node.id()}
-         |  Node Type = ${node.label()}
-         |  Parent = ${parentsList.mkString(",\n\t")}
-         |  Children = ${childrenList.mkString(",\n\t ")}
-         |  Code = ${nodeContext.property(Properties.Code)}
+         |Node ID: ${node.id()}
+         |Node Type: "${node.label()}",
+         |Parents: {${parentsList.mkString(",\n\t")}\n},
+         |Children: {${childrenList.mkString(",\n\t ")}\n},
          |""".stripMargin
+
     text
   }
 }
--- cpg-trimmer/src/main/scala/io/joern/CPGTrimmer/CPGTrimmer.scala
+++ cpg-trimmer/src/main/scala/io/joern/CPGTrimmer/CPGTrimmer.scala
@@ -1,20 +1,20 @@
 package io.joern.CPGTrimmer
 
-import io.shiftleft.semanticcpg.language.*
+// import io.shiftleft.semanticcpg.language.*
 import io.shiftleft.semanticcpg.layers.{LayerCreator, LayerCreatorContext, LayerCreatorOptions}
 import io.shiftleft.codepropertygraph.generated.Properties
-import io.shiftleft.codepropertygraph.generated.NodeTypes
-
+// import io.shiftleft.codepropertygraph.generated.NodeTypes
+import java.io.{FileWriter, PrintWriter, File}
+import scala.util.Using
 
 object CPGTrimmer {
-  val overlayName = "CPG Trimmer"
+  val overlayName = "cpg-trimmer"
   val description = "Logs context for unnecessary empty nodes in a Joern CPG"
 
   def defaultOpts = CPGTrimmerOpts(".")
 }
 
-case class CPGTrimmerOpts(var pathToRepo: String)
-  extends LayerCreatorOptions {}
+case class CPGTrimmerOpts(var outputDir: String) extends LayerCreatorOptions {}
 
 class CPGTrimmer(options: CPGTrimmerOpts) extends LayerCreator {
   override val overlayName: String = CPGTrimmer.overlayName
@@ -22,9 +22,33 @@ class CPGTrimmer(options: CPGTrimmerOpts) extends LayerCreator {
 
   override def create(context: LayerCreatorContext): Unit = {
     val cpg = context.cpg
-    val blocks = cpg.graph.nodes(NodeTypes.BLOCK).toList
-    val emptyBlocks = blocks.filter(node => node.property(Properties.Code).equals("<empty>"))
-    val logger = new CPGTrimLogger(emptyBlocks, cpg)
-    logger.logNodes()
+    val empty = cpg.graph.nodesWithProperty(Properties.Code.toString, "<empty>").toList
+    // val blocks = cpg.graph.nodes(NodeTypes.BLOCK).toList
+    // val emptyBlocks = blocks.filter(node => node.property(Properties.Code).equals("<empty>"))
+
+    if (empty.nonEmpty) {
+      val logDir = new File(options.outputDir)
+      logDir.mkdirs()
+
+      val logFilename: String = options.outputDir + "/" + overlayName + ".log"
+      println(s"Writing empty block node details to ${logFilename}")
+
+      Using(new PrintWriter(new FileWriter(logFilename))) { writer =>
+        writer.println(s"Empty Block List - ${empty.size} empty blocks")
+        writer.println("=" * 50)
+
+        val logger = new CPGTrimLogger(empty, cpg, Some(writer))
+        logger.logNodes()
+
+        writer.println("=" * 50)
+      }.recover {
+        case ex => println(s"Failed to write log: ${ex.getMessage}")
+      }
+
+      println(s"Details written to: ${logFilename}")
+
+    } else {
+      println("No empty nodes found.")
+    }
   }
 }
